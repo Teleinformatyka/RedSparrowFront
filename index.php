2f484dc94fa8959430c38aedb26806be095f8398
Konrad Kusnierz <iryont@gmail.com>
2014-11-17 22:28:24 +0100

    Render new UI

--- index.php
+++ index.php
@@ -11,15 +11,15 @@
   $f3->route('GET /',
     function($f3) {
       $f3->set('logo', 'redsparrow.png');
-      $f3->set('content','index.htm');
-      echo View::instance()->render('layout.htm');
+      //$f3->set('content','index.htm');
+      echo View::instance()->render('index.html');
     }
   );
 
-  $f3->route('GET /subpage',
+  $f3->route('GET /login',
     function($f3) {
-      $f3->set('content','subpage.htm');
-      echo View::instance()->render('layout.htm');
+      //$f3->set('content','subpage.htm');
+      echo View::instance()->render('login.htmy');
     }
   );
 
