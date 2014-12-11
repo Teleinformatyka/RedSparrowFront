1f89ee572f20111cef10db448f5de5c0a897327c
Konrad <iryont@gmail.com>
2014-12-11 20:32:55 +0100

    Fixes

--- controllers/register.php
+++ controllers/register.php
@@ -8,27 +8,23 @@ class Register {
   public static function handle($f3){
     if($f3->exists('POST.submit')){
       if($f3->get('POST.password1') != $f3->get('POST.password2')){
-        return Register::action($f3, Error::PASSWORDS_NOT_EQUAL);
+        $f3->set('error_code', Error::PASSWORDS_NOT_EQUAL);
+      } else {      
+        /* Create user */
+        $user = User::create($f3->get('POST.login'), $f3->get('POST.password1'), $f3->get('POST.email'), $f3->get('POST.name'), $f3->get('POST.surname'));
+        
+        /* Register user with save function */;
+        $f3->set('error_code', $user->save());
       }
-      
-      /* Create user */
-      $user = User::create($f3->get('POST.login'), $f3->get('POST.password1'), $f3->get('POST.email'), $f3->get('POST.name'), $f3->get('POST.surname'));
-      
-      /* Register user with save function */
-      return Register::action($f3, $user->save());
     }
     
-    /* Register form */
-    return Register::render($f3);
-  }
-  
-  public static function action($f3, $code){
-    $f3->set('error_code', $code);
-    return Register::render($f3);
-  }
-  
-  public static function render($f3){
-    echo View::instance()->render('register.html');
+    if($f3->get('error_code') == Error::NO_ERROR){
+      /* Re-route to login form */
+      $f3->reroute('/login');
+    } else {  
+      /* Register form */
+      echo View::instance()->render('register.html');
+    }
   }
 }
 
