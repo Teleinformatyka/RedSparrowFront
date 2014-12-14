86bb2fcba75a2c460169a535c527e1391ccb8851
vanluke22 <vanluke@op.pl>
2014-12-14 09:04:47 +0100

    Pass data from controller to html page #35

--- controllers/profile.php
+++ controllers/profile.php
@@ -2,9 +2,29 @@
 
 require_once('lib/zmq.php');
 
+require_once('lib/error.php');
+
+require_once('models/user.php');
+
+// Profile class.
 class Profile {
-  public static function handle($f3){
+
+   // Default constructor.
+   public function __construct(){
+   	 // Get user from session.
+   	 
+   }
+
+  // handle function.
+  public static function handle($f3) {
      $f3->set('variable', 'Test');
+     // $f3->set('username',  $f3->get('SESSION.user'));
+    // $f3->set('level','SESSION.user.level');
+    //$f3->set('email','SESSION.user.email');
+    $f3->set('username',  'Jan Kowalski');
+    $f3->set('level','Promotor');
+    $f3->set('email','kowalski@jan.com');
+     // render profile.html.
     echo View::instance()->render('profile.html');
   }
 }
