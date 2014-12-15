dd38a6e71d6561c29fcbce5ef9e2948d0663f137
TomaszW <tomek@sofect.com>
2014-12-15 18:13:58 +0100

    UI Fixes (move JS from footer to header).

--- controllers/index.php
+++ controllers/index.php
@@ -1,8 +1,8 @@
 <?php
-use \Session as Session;
 
 class Index {
   public static function handle($f3){
+    new Session();
     if($f3->exists('SESSION.verified')){
 		echo View::instance()->render('headerTemplate.html');
 		echo View::instance()->render('index.html');
