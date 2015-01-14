2339c187cb43cf1aa8f106c361499953dee728da
Marcin Kaciuba <aldor007@gmail.com>
2015-01-14 23:11:26 +0100

    update  for zmq

--- examples/register_login.php
+++ examples/register_login.php
@@ -1,14 +1,14 @@
 <?php
-require_once('../lib/zmq.php');
+require_once('../lib/zmqmessage.php');
 
 
   $message = array(
-      'id' => 1,
+      'id' => 214,
       'method' => 'register',
       'params' => array('login' => 'aldor', 'password' => 'aldor', 'email' => 'aldor', 'name' => 'aldor', 'surname' => 'aldor')
   );
 
-  $zmq = new ZMQMessage('tcp://178.238.41.170:5555');
+  $zmq = new ZMQMessage('tcp://aldorhost.pl:5555');
 
   // Send message to the server
   $zmq->send($message);
@@ -18,8 +18,29 @@ require_once('../lib/zmq.php');
       'method' => 'login',
       'params' => array('login' => 'aldor', 'password' => 'aldor')
   );
-  echo "\n----------------------\n";
+  echo "\n---------LOGIN-------------\n";
+  $zmq->send($message);
+  print_r($zmq->recv());
+ $message = array(
+     'jsonrpc' => '2.0',
+     'method' => 'usermethods-edit_user',
+     'params' => array('columnName' => 'password', 'value' => md5('trombka2'), 'userId' => 6),
+     'id' => 6621
+   );
+  echo "\n---------usermethods-edit_user-------------\n";
   $zmq->send($message);
   print_r($zmq->recv());
 
 
+
+
+  $user_del = array(
+      'id' => 1,
+      'method' => 'register',
+      'params' => array('login' => 'aldor', 'password' => 'aldor', 'email' => 'aldor', 'name' => 'aldor', 'surname' => 'aldor')
+  );
+
+
+  echo "\n---------register-------------\n";
+  $zmq->send($user_del);
+  print_r($zmq->recv());
