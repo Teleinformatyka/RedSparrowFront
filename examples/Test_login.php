eed229057dcaa8721c007d772712595eb7545b6d
buli <karolbuli1@gmail.com>
2015-01-17 13:59:09 +0100

    Update Test_login.php

--- examples/Test_login.php
+++ examples/Test_login.php
@@ -1,13 +1,9 @@
 
 <?php
 require_once('../lib/zmqmessage.php');
- echo "\n---------REGISTR-------------\n";
- $message = array(
-'id' => 214,
-'method' => 'register',
-'params' => array('login' => 'aldor', 'password' => 'aldor', 'email' => 'aldor', 'name' => 'aldor', 'surname' => 'aldor')
-);
 
+ $zmq = new ZMQMessage('tcp://aldorhost.pl:5555');
+ echo "\n---------REGISTR-------------\n";
 
  $message = array(
 'id' => 1,
