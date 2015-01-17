dbb3f7a9019df02814bc9d631e170bab8bd6de1d
buli <karolbuli1@gmail.com>
2015-01-17 14:01:55 +0100

    Update Test_login.php

--- examples/Test_login.php
+++ examples/Test_login.php
@@ -15,7 +15,7 @@ print_r($zmq->recv());
  $message = array(
 'id' => 214,
 'method' => 'register',
-'params' => array('login' => 'buli', 'password' => 'buli', 'email' => 'buli@pp.pl', 'name' => 'buli', 'surname' => 'buli')
+'params' => array('login' => 'buli', 'password' => 'buli', 'email' => 'budddli@pp.pl', 'name' => 'buli', 'surname' => 'buli')
 );
  $zmq->send($message);
 print_r($zmq->recv());
