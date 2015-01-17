9edb37ff8b93577b268cd076f9c8c2d3e18bc1da
buli <karolbuli1@gmail.com>
2015-01-17 13:54:26 +0100

    Update Test_login.php

--- examples/Test_login.php
+++ examples/Test_login.php
@@ -1,15 +1,19 @@
 
 <?php
 require_once('../lib/zmqmessage.php');
-
-$message = array(
+echo "logowanie"
+ $message = array(
+'id' => 1,
+'method' => 'login',
+'params' => array('login' => 'buli', 'password' => 'buli')
+);
+ $zmq->send($message);
+print_r($zmq->recv());
+ $message = array(
 'id' => 214,
 'method' => 'register',
-'params' => array('login' => 'aldor', 'password' => 'aldor', 'email' => 'aldor', 'name' => 'aldor', 'surname' => 'aldor')
+'params' => array('login' => 'buli', 'password' => 'buli', 'email' => 'buli@pp.pl', 'name' => 'buli', 'surname' => 'buli')
 );
-$zmq = new ZMQMessage('tcp://aldorhost.pl:5555');
-// Send message to the server
-$zmq->send($message);
+ $zmq->send($message);
 print_r($zmq->recv());
-
 ?>
