c4bdb667bb2881c84752c56705939135595c994e
buli <karolbuli1@gmail.com>
2015-01-17 14:03:35 +0100

    Update Test_login.php

--- examples/Test_login.php
+++ examples/Test_login.php
@@ -3,25 +3,27 @@
 require_once('../lib/zmqmessage.php');
 
  $zmq = new ZMQMessage('tcp://aldorhost.pl:5555');
- echo "\n---------REGISTR-------------\n";
+ echo "\n---------Login-------------\n";
 
  $message = array(
-'id' => 1,
+'id' => 300,
 'method' => 'login',
 'params' => array('login' => 'buli', 'password' => 'buli')
 );
  $zmq->send($message);
 print_r($zmq->recv());
+ echo "\n---------LOGIN end-------------<br/>";
+  echo "\n---------REGISTER -------------<br/>";
  $message = array(
-'id' => 214,
+'id' => 300,
 'method' => 'register',
 'params' => array('login' => 'buli', 'password' => 'buli', 'email' => 'budddli@pp.pl', 'name' => 'buli', 'surname' => 'buli')
 );
  $zmq->send($message);
 print_r($zmq->recv());
-
+ echo "\n---------REGISTER end-------------<br/>";
  $message = array(
-'id' => 214,
+'id' => 300,
 'method' => 'login',
 'params' => array('login' => 'buli', 'password' => 'buli')
 );
