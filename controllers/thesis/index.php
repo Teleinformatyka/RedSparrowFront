ba37bdd6d5efebfe4a39d6051a5d93910c3b9427
Tomek <tomaszziebiec@gmail.com>
2015-01-20 00:31:40 +0100

    Modify ThesesBase, getTheses by use ID

--- controllers/thesis/index.php
+++ controllers/thesis/index.php
@@ -1,15 +1,30 @@
-<?php namespace Thesis; use \View as View; use \Session as Session; use \Template as Template; use \StaticClass as StaticClass;
+<?php namespace Thesis; use \View as View; use \Session as Session; use \Template as Template; use \StaticClass as StaticClass; use \Thesis as ThesisModel;
 
-class Index extends StaticClass {
-  public static function handle($f3){
-    new Session();
-    
-    if($f3->exists('SESSION.verified')){
-      echo Template::instance()->render('showThesesBase.html');
-    } else {
-      $f3->reroute('/login');
+class Index extends StaticClass
+{
+    public static function handle($f3)
+    {
+        new Session();
+
+        if ($f3->get('SESSION.verified')) {
+            $login = $f3->get('SESSION.login');
+            $hash = $f3->get('SESSION.password');
+            $user = User::load($login, $hash, false);
+
+            if ($user->exists()) {
+                /* Template variables */
+                $theseesBase = ThesisModel::getListOfThesesByUserId($user->get('id'));
+                $f3->set('theseesBase', $theseesBase);
+
+                echo Template::instance()->render('showThesesBase.html');
+            } else {
+                $f3->reroute('/logout');
+            }
+        } else {
+            /* You are not logged in */
+            $f3->reroute('/login');
+        }
     }
-  }
 }
 
 ?>
